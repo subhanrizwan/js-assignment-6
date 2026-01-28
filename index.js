ef050b18bf885312ff098cfad7b416d9fb897d68
subhan ahmed <99755580+subhanrizwan@users.noreply.github.com>
2026-01-28 05:37:13 +0500

    Update prompt message for food entry

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 document.write("<h1>"+ "OUR RESTURANT"+ "</h1>")
 var fd =["donut","softdrink", "candy","samosa","roll","zinger","pizaa"]
 var food=["raita", "donut","softdrink", "candy","samosa","roll","zinger","pizaa"]
-var prm = prompt("enter")
+var prm = prompt("enter new food")
 for(var i = 0; i < food.length; i++){
     if(prm===food[i]){
         document.write("<h2>" + food[i] + " is available" + "</h2>")
-
-
         for( var j = 0; j < fd.length; j++ ){
             document.write("<h2>" +(j + 1)+ fd[j] + " is also available" + "</h2>")
-            
-        
-     
     }
 
 }
